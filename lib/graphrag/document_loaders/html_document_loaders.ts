9b208bd51f21eea185f4e55e3cb0ee19d8b46e0f
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2024-07-19 21:09:08 +0530

    chore: added graph rag internal implementation '

---
+++ lib/graphrag/document_loaders/html_document_loaders.ts
@@ -0,0 +1,6 @@
+(() => {
+  // Convert PDF to Text units
+  // Convert Text unites to vector encodings using lama index or langchain
+  // configure pinecon DB
+  // store vector encodings to pinecone DB
+})();
